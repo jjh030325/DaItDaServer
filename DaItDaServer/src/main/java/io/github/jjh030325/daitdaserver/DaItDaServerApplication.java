57be56d2bb46724fa9efb8785c294be18a020830
wpqlks7 <wpqlks7@gmail.com>
2025-06-29 19:56:50 +0900

    초기 세팅

---
+++ DaItDaServer/src/main/java/io/github/jjh030325/daitdaserver/DaItDaServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.jjh030325.daitdaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaItDaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaItDaServerApplication.class, args);
+    }
+
+}
---
+++ DaItDaServer/src/test/java/io/github/jjh030325/daitdaserver/DaItDaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jjh030325.daitdaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaItDaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
